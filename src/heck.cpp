b03fe2e9157eab05975ee0a9c77d503311adeb9c
ImpostorBoy <impostorboy@yandex.ru>
2026-06-23 00:39:52 +0700

    rewrite in classes Sigma & Amogus

--- src/heck.cpp
+++ src/heck.cpp
@@ -1,5 +1,6 @@
 module;
 #include <SDL3/SDL.h>
+#include <SDL3/SDL_properties.h>
 #include <bgfx/bgfx.h>
 #include <expected>
 #include <iostream>
@@ -7,6 +8,7 @@ module;
 #include <string_view>
 export module heck;
 
+//SDL
 export class Sigma {
 private:
   Sigma(std::string_view title, int x, int y, SDL_Window *buzz)
@@ -14,9 +16,10 @@ private:
 
 public:
   ~Sigma() {
-    bgfx::shutdown();
-    SDL_DestroyWindow(buzz);
-    SDL_Quit();
+    if (buzz != nullptr) {
+      SDL_DestroyWindow(buzz);
+      SDL_Quit();
+    }
   }
 
   static std::expected<Sigma, std::string>
@@ -41,11 +44,104 @@ public:
 
   Sigma(const Sigma &) = delete;
   Sigma &operator=(const Sigma &) = delete;
-  Sigma(Sigma &&) = default;
+  Sigma(Sigma&& other)
+  : title(std::move(other.title)), x(other.x), y(other.y), buzz(other.buzz) {
+    other.buzz = nullptr;
+  }
   Sigma &operator=(Sigma &&) = default;
 
 private:
   std::string title;
   int x, y;
   SDL_Window *buzz = nullptr;
+};
+
+//bgfx
+export class Amogus {
+private:
+  SDL_Window *buzz = nullptr;
+  int x, y;
+  bgfx::RendererType::Enum goonerType;
+
+  Amogus(SDL_Window* buzz, int x, int y, bgfx::RendererType::Enum goonerType)
+  : buzz(buzz), x(x), y(y), goonerType(goonerType) {}
+public:
+  ~Amogus() {if (buzz) bgfx::shutdown();}
+
+  static std::expected<Amogus, std::string>
+  rizzing(SDL_Window *buzz, int w, int h, bgfx::RendererType::Enum goonerType) {
+      bgfx::PlatformData pld{};
+      pld.nwh = nullptr;
+      pld.ndt = nullptr;
+      pld.context = nullptr;
+      pld.backBuffer = nullptr;
+      pld.backBufferDS = nullptr;
+      pld.type = bgfx::NativeWindowHandleType::Wayland;
+
+      SDL_PropertiesID props = SDL_GetWindowProperties(buzz);
+      void *wl_display = SDL_GetPointerProperty(
+          props, SDL_PROP_WINDOW_WAYLAND_DISPLAY_POINTER, nullptr);
+      void *wl_surface = SDL_GetPointerProperty(
+          props, SDL_PROP_WINDOW_WAYLAND_SURFACE_POINTER, nullptr);
+
+      if (wl_display && wl_surface) {
+        pld.ndt = wl_display;
+        pld.nwh = wl_surface;
+      } else {
+        pld.type = bgfx::NativeWindowHandleType::Default;
+        void *x11_display = SDL_GetPointerProperty(
+            props, SDL_PROP_WINDOW_X11_DISPLAY_POINTER, nullptr);
+        void *x11_window = SDL_GetPointerProperty(
+            props, SDL_PROP_WINDOW_X11_WINDOW_NUMBER, nullptr);
+        if (x11_display && x11_window) {
+          pld.ndt = x11_display;
+          pld.nwh = x11_window;
+        } else {
+          return std::unexpected("no wayland or x11 props");
+        }
+      }
+
+      bgfx::Init init;
+      init.type = goonerType;
+      init.resolution.width = w;
+      init.resolution.height = h;
+      init.resolution.reset = BGFX_RESET_VSYNC;
+      init.platformData = pld;
+
+      if (!bgfx::init(init)) {
+          return std::unexpected("bgfx::init edged");
+      }
+
+      bgfx::setViewClear(0, BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH, 0x303030ff, 1.0f, 0);
+      bgfx::setViewRect(0, 0, 0, w, h);
+
+      return std::expected<Amogus, std::string>(Amogus(buzz, w, h, goonerType));
+  }
+
+  void frame() {bgfx::touch(0); bgfx::frame();}
+  void resize(int w, int h) {
+    bgfx::reset(w, h, BGFX_RESET_VSYNC);
+    bgfx::setViewRect(0, 0, 0, w, h);
+    x = w;
+    y = h;
+  }
+
+  Amogus(const Amogus&) = delete;
+  Amogus& operator=(const Amogus&) = delete;
+
+  Amogus(Amogus&& other) noexcept
+  : buzz(other.buzz), x(other.x), y(other.y), goonerType(other.goonerType) {
+      other.buzz = nullptr;
+  }
+
+  Amogus& operator=(Amogus&& other) noexcept {
+    if (this != &other) {
+      buzz = other.buzz;
+      x = other.x;
+      y = other.y;
+      goonerType = other.goonerType;
+      other.buzz = nullptr;
+    }
+    return *this;
+  }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -11,48 +11,14 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
     return 1;
   }
   Sigma sigma = std::move(cringe.value());
-  SDL_Window *buzz = sigma.getWindow();
 
-  bgfx::PlatformData pld;
-  pld.nwh = nullptr;
-  pld.ndt = nullptr;
-  pld.context = nullptr;
-  pld.backBuffer = nullptr;
-  pld.backBufferDS = nullptr;
-  pld.type = bgfx::NativeWindowHandleType::Wayland;
-
-  SDL_PropertiesID props = SDL_GetWindowProperties(buzz);
-  void *wl_display = SDL_GetPointerProperty(
-      props, SDL_PROP_WINDOW_WAYLAND_DISPLAY_POINTER, nullptr);
-  void *wl_surface = SDL_GetPointerProperty(
-      props, SDL_PROP_WINDOW_WAYLAND_SURFACE_POINTER, nullptr);
-  if (wl_display && wl_surface) {
-    pld.ndt = wl_display;
-    pld.nwh = wl_surface;
-  } else {
-    std::cerr << "E666: no props" << std::endl;
-    SDL_DestroyWindow(buzz);
-    SDL_Quit();
+  std::expected<Amogus, std::string> drake =
+      Amogus::rizzing(sigma.getWindow(), 800, 600, bgfx::RendererType::Vulkan);
+  if (!drake) {
+    std::cerr << "E666: " << drake.error() << std::endl;
     return 1;
   }
-
-  bgfx::Init init;
-  init.type = bgfx::RendererType::Vulkan;
-  init.resolution.width = 800;
-  init.resolution.height = 600;
-  init.resolution.reset = BGFX_RESET_VSYNC;
-  init.platformData = pld;
-
-  if (!bgfx::init(init)) {
-    std::cerr << "E228: bgfx::init failed" << std::endl;
-    SDL_DestroyWindow(buzz);
-    SDL_Quit();
-    return 1;
-  }
-
-  bgfx::setViewClear(0, BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH,
-      0x303030ff, 1.0f, 0);
-  bgfx::setViewRect(0, 0, 0, 800, 600);
+  Amogus amogus = std::move(drake.value());
 
   bool gooning = true;
   while (gooning) {
@@ -67,16 +33,11 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
         }
       }
       if (event.type == SDL_EVENT_WINDOW_RESIZED) {
-        bgfx::setViewRect(0, 0, 0, event.window.data1, event.window.data2);
+        amogus.resize(event.window.data1, event.window.data2);
       }
     }
-    bgfx::touch(0);
-    bgfx::frame();
+    amogus.frame();
   }
-
-  bgfx::shutdown();
-  SDL_DestroyWindow(buzz);
-  SDL_Quit();
 
   return 0;
 }
