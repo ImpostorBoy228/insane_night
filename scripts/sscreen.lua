c4db1eb9db018c94f7387c9f86ddb3f6eff7f17e
ImpostorBoy <impostorboy@yandex.ru>
2026-06-28 13:55:42 +0700

    center the div

--- scripts/sscreen.lua
+++ scripts/sscreen.lua
@@ -1,4 +1,4 @@
-setFullscreen(true)
+setFullscreen(false)
 
 local ui = addUILayer("ui")
 
@@ -7,7 +7,7 @@ if bg.idx ~= 65535 then
     ui:addImageF(getImageGooner(), bg, 0, 0, 1, 1, 0xffffffff, -1)
 end
 
-local r = ui:addRectF(getRectGooner(), 0.5, 0.40, 0.16, 0.07, 0xffffffff, 0)
+local r = ui:addRectF(getRectGooner(), 0.5 - 0.16/2, 0.5 - 0.07/2, 0.16, 0.07, 0xffffffff, 0)
 r:onClick(function()
     print("rect clicked")
 end)
